1d0992c82955f6f3d4fa17588bd9aff9e10dd7fb
Jake King <JakeKing@users.noreply.github.com>
2017-01-19 19:31:16 -0800

    Added nodejs PoC app.

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/post/', function(req, res) {
+   var sn = JSON.stringify(req.body);
+   console.log( req.connection.remoteAddress + " " + req.headers['user-agent'] +  " " + sn );
+   res.send("OK");
+});
+
+app.get('/', function(req, res) {
+  res.sendFile('index.html', { root: __dirname });
+  //console.log("GET request to / from  " + req.connection.remoteAddress);
+});
+
+app.get('/payload.js', function(req, res) {
+  res.sendFile('payload.js', { root: __dirname });
+  //console.log("GET request to /payload.js from " + req.connection.remoteAddress);
+});
+
+app.listen(7002);
